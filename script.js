6f72d342851043da66cf3fdfbf407c2bb330fc18
mari2791 <marijalaghonia@gmail.com>
2025-05-19 11:23:49 +0400

    colsole.log abd console.warn added to js

--- script.js
+++ script.js
@@ -1,2 +1,6 @@
-console.log("Hello World");
-console.warn("გაფრთხილება")
+console.log("Name is : Mariana");
+console.log("Surname is : Jalagonia");
+console.log("Age : 100");
+console.warn("Email : marijalaghonia@gmail.com")
+console.warn("Password : 123456")
+console.log("Phone : 555-555-555");
