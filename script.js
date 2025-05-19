60911845af41a03e69d4b17f09063c76a4002711
mari2791 <marijalaghonia@gmail.com>
2025-05-19 11:24:55 +0400

    minor edits made in js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Name is : Mariana");
 console.log("Surname is : Jalagonia");
 console.log("Age : 100");
-console.warn("Email : marijalaghonia@gmail.com")
-console.warn("Password : 123456")
+console.warn("Email : marijalaghonia@gmail.com");
+console.warn("Password : 123456");
 console.log("Phone : 555-555-555");
